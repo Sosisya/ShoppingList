463f8aaa43b6a5ab460cc97a775625e5aa1dcaec
Sosisya <khaj-luizkin@yandex.ru>
2024-02-28 15:26:10 +0700

    Create ShopListRepository and add to domain

--- app/src/main/java/com/example/shoppinglist/domain/AddShopItemUseCase.kt
+++ app/src/main/java/com/example/shoppinglist/domain/AddShopItemUseCase.kt
@@ -1,7 +1,7 @@
 package com.example.shoppinglist.domain
 
-class AddShopItemUseCase {
-    fun addShopItem(shopItem: ShopItem){
-        TODO()
+class AddShopItemUseCase(private val shopListRepository: ShopListRepository) {
+    fun addShopItem(shopItem: ShopItem) {
+        shopListRepository.addShopItem(shopItem)
     }
 }
